190165a6e49b2b41ff86c97a0545674ec8d118a0
nicooooo <nicooooo@outlook.fr>
2016-05-11 06:27:28 -0400

    debut de correction

--- films.php
+++ films.php
@@ -19,7 +19,7 @@
       $conn=mysqli_connect("sql7.freemysqlhosting.net","sql7118747","xXGqKMPhbi","sql7118747");
       $result = mysqli_query($conn,"SELECT distinct nom From  Artiste ");
        while($artiste=mysqli_fetch_assoc($result)){
-       echo"<OPTION>".$artiste['nom'];
+       echo"<OPTION>".$artiste['nom']; //il manque un "</option>"
        }
       
       ?>
@@ -35,27 +35,28 @@ if(!$link){
   die("<p>connexion impossible</p>");
 }
       
+      
 $result = mysqli_query($link,"SELECT titre,annee,genre,nom FROM Film , Artiste where Artiste.idArtiste=Film.idMes");
     
 if($result){
-  echo "<table>";
-  echo "<tr>";
-    echo "<th>titre</th>";
+     echo "<table>";
+     echo "<tr>";
+     echo "<th>titre</th>";
      echo"<th>annee</th>";
      echo"<th>genre</th>";
      echo"<th>nom</th>";
-    echo "</tr>";
-  while($film=mysqli_fetch_assoc($result)){
-    echo "<tr>";
-  echo "<td>".$film['titre']."</td>";
-  echo"<td>".$film['annee']."</td>";
-  echo "<td>".$film['genre']."</td>";
-    echo"<td>".$film['nom']."</td>";
-    echo "</tr>";
-  }
- echo "</table>";
-} else
-die("<p>Erreur dans l'éxécution de la requête.</p>");
+     echo "</tr>";
+      while($film=mysqli_fetch_assoc($result)){
+       echo "<tr>";
+       echo "<td>".$film['titre']."</td>";
+       echo"<td>".$film['annee']."</td>";
+       echo "<td>".$film['genre']."</td>";
+       echo"<td>".$film['nom']."</td>";
+       echo "</tr>";
+           }
+   echo "</table>";
+   }   else
+       die("<p>Erreur dans l'éxécution de la requête.</p>");
      
     ?>  
       
